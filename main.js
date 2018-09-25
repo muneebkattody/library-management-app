f5e7406c5de8786f5fe04ba76ad57b173bc2e7ca
muneebkattody <muneebkattody@users.noreply.github.com>
2018-09-25 22:17:54 +0530

    added UI

---
+++ main.js
@@ -0,0 +1,33 @@
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+
+// SET ENV
+process.env.NODE_ENV = 'development';
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+let mainWindow;
+let addWindow;
+
+// Listen for app to be ready
+app.on('ready', function(){
+  // Create new window
+  mainWindow = new BrowserWindow({
+    transparent: false,
+    frame: false,
+    fullscreen: false,
+    titleBarStyle: 'hidden'
+  });
+  // Load html in window
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes:true
+  }));
+  // Quit app when closed
+  mainWindow.on('closed', function(){
+    app.quit();
+  });
+  
+});
